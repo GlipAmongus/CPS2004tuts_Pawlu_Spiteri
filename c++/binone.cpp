4de6733de023821fce304932da1ae1309c688f3b
GlipAmongus <pawlunumber2@gmail.com>
2024-10-22 23:48:27 +0200

    working error/exception handling

--- c++/binone.cpp
+++ c++/binone.cpp
@@ -11,13 +11,46 @@ struct Tuple {
 };
 
 int main(){
+    //Fixed array on the heap
     Tuple* tups = new Tuple[3];  
 
+    //Read from console (operand1 op operand2)
     for(int i = 1; i <= 3; i++){
         cout << "Give me expression "<< i <<"!:" << endl;
         cin >> tups[i].opnd1 >> tups[i].op >> tups[i].opnd2;
-        // Implement exception handling for incorrect types
+
+        try{
+            if(cin.fail()){
+                cin.clear();
+                cin.ignore(1000, '\n');
+                throw "Fail1: incorrect types";
+            }
+
+            switch(tups[i].op){
+                case '+':
+                    cout << "valid" << endl;
+                    break;
+                case '-':
+                    cout << "valid" << endl;
+                    break;
+                case '/':
+                    cout << "valid" << endl;
+                    break;
+                case '*':
+                    cout << "valid" << endl;
+                    break;
+                default:
+                    throw "Fail2: incorrect operator";
+            }
+        }
+        catch (const char * s){
+                cout << s << endl;
+                cout << "Invalid input. try again!" << endl;
+                i--;
+                continue;
+        }
     }
+
     for(int i = 1; i <= 3; i++){
         cout << tups[i].opnd1 <<" "<< tups[i].op <<" "<< tups[i].opnd2 << endl;
     }
